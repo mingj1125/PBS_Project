6714b00a465b329a1f4c537a730da58c8f7e76ef
mingj1125 <limingjiecaitie@gmail.com>
2022-12-11 21:00:38 +0100

    added alternative mesh position initialization for particular scenes

--- script/mesh_collision.py
+++ script/mesh_collision.py
@@ -28,6 +28,40 @@ def init_collision_bodies():
             mesh_position[idx] += ti.math.vec3([15., 1., 30.]) + b_idx*ti.math.vec3([0., 0., 15.])
             idx += 1
 
+def init_collision_bodies_lighthouse_scene():
+    # init rotation
+    for b_idx in range(num_collision_bodies):
+         mesh_rotation[b_idx] = ti.Vector([0., (math.pi/4)*b_idx, 0.])
+    # create mesh particles
+    idx = 0
+    for b_idx in range(num_collision_bodies):
+        mesh = meshio.read(mesh_names[b_idx])
+        mesh_points = mesh.points
+        R = rotation_mat(mesh_rotation[b_idx][0],mesh_rotation[b_idx][1],mesh_rotation[b_idx][2])
+        print_mesh_info(b_idx, mesh_points.shape[0], R)
+        for i in range(mesh_points.shape[0]):
+            mesh_position[idx] = ti.Vector(R @ np.array(mesh_points[i]))
+            mesh_position[idx] *= 10* (1-b_idx*0.65)
+            mesh_position[idx] += ti.math.vec3([20., 1., 14.]) + b_idx*ti.math.vec3([-6., 0., 15.])
+            idx += 1            
+
+def init_collision_bodies_bathroom_scene():
+    # init rotation
+    for b_idx in range(num_collision_bodies):
+         mesh_rotation[b_idx] = ti.Vector([0., math.pi/2.- (0.3+math.pi)*b_idx, 0.])
+    # create mesh particles
+    idx = 0
+    for b_idx in range(num_collision_bodies):
+        mesh = meshio.read(mesh_names[b_idx])
+        mesh_points = mesh.points
+        R = rotation_mat(mesh_rotation[b_idx][0],mesh_rotation[b_idx][1],mesh_rotation[b_idx][2])
+        print_mesh_info(b_idx, mesh_points.shape[0], R)
+        for i in range(mesh_points.shape[0]):
+            mesh_position[idx] = ti.Vector(R @ np.array(mesh_points[i]))
+            mesh_position[idx] *= 8 * (1-b_idx/2*0.3)
+            mesh_position[idx] += ti.math.vec3([22., 0., 40.]) + b_idx*ti.math.vec3([-7., 0., -20./(b_idx+1)])
+            idx += 1                        
+
 ## print info
 def print_mesh_info(b_idx, num, R):
     print("Insert Mesh vtk-file: "+mesh_names[b_idx]+"\n"
