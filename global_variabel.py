540d92ed70c52bc5c571b83d6f81dd96684c2992
mingj1125 <limingjiecaitie@gmail.com>
2022-12-11 23:57:33 +0100

    added particle distribution for bathroom scene

--- global_variabel.py
+++ global_variabel.py
@@ -31,6 +31,10 @@ tol = 1e-6
 num_particles_x = 10
 num_particles_y = 35
 num_particles_z = 50
+# bathroom scene
+# num_particles_x = 30
+# num_particles_y = 15
+# num_particles_z = 30
 num_particles = num_particles_x * num_particles_y * num_particles_z
 num_fluid_particles = num_particles
 max_num_particles_per_cell = 100
@@ -61,9 +65,9 @@ num_collision_bodies = 2
 num_bodies_particles = 0
 mesh_sizes = []
 # mesh input for bunnies
-mesh_names = ["meshes/bunny_dense.vtk","meshes/bunny_dense.vtk"]
+# mesh_names = ["meshes/bunny_dense.vtk","meshes/bunny_dense.vtk"]
 # mesh input for bathroom scene
-# mesh_names = ["meshes/bathtub_volume.vtk","meshes/bunny.vtk","meshes/bunny.vtk"]
+mesh_names = ["meshes/bathtub_volume.vtk","meshes/bunny.vtk","meshes/bunny.vtk"]
 # mesh input for lighthouse scene
 # mesh_names = ["meshes/lighthouse.vtk","meshes/sea_arch.vtk"]
 mesh_sizes = []
@@ -103,6 +107,21 @@ corrK = 0.001
 neighbor_radius = h_ * 1.05
 # solid density scaling eq.27 unified particle system
 s = 0.5
+
+# dynamic balls params
+num_collision_balls = 4
+collision_ball_radius = 1.5
+stablization_iters = 8
+
+max_num_balls_per_cell = 50
+max_num_virtual_neighbors = 50
+virtual_particle_neighbors_dist = (particle_radius*4*h_)
+neighborhood_particle_off = math.floor(neighbor_radius/virtual_particle_neighbors_dist)
+
+bool_ball = True
+if  num_collision_balls == 0:
+    bool_ball = False
+    num_collision_balls = 1
 
 poly6_factor = 315.0 / 64.0 / math.pi
 spiky_grad_factor = -45.0 / math.pi
